7f7c1cc2568004cba5bc6afb0f05011367c7c22e
Jonathan Ostrander <jonathanost@gmail.com>
2021-11-30 18:56:29 -0500

    feat: add day-runner to automatically pull input for the day when running from vs code

---
+++ src/main/scala/aoc/Day.scala
@@ -0,0 +1,5 @@
+package aoc
+
+trait Day {
+  def run(): String
+}
--- src/main/scala/aoc/Day01.scala
+++ src/main/scala/aoc/Day01.scala
@@ -1,5 +1,5 @@
 package aoc
 
-object Day01 extends InputReader(1) {
-  println(input.mkString("\n"))
+object Day01 extends InputReader(1) with Day {
+  override def run(): String = input.mkString("\n")
 }
