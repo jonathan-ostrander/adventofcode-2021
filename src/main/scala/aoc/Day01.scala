a96569e0d6280f66b72d466393a0f2081e9a2844
Jonathan Ostrander <jonathanost@gmail.com>
2021-12-01 00:33:15 -0500

    refactor: more concise day 1

--- src/main/scala/aoc/Day01.scala
+++ src/main/scala/aoc/Day01.scala
@@ -3,11 +3,10 @@ package aoc
 object Day01 extends InputReader(1) with Day {
   val ints = input.map(_.toInt)
 
-  def slidingIncrease(windowSize: Int): Int = {
-    ints.sliding(windowSize + 1).foldLeft(0) {
-      case (i, window) => if (window.head < window.last) i + 1 else i
-    }
-  }
+  def slidingIncrease(windowSize: Int): Int =
+    ints
+      .sliding(windowSize + 1)
+      .count(window => window.head < window.last)
 
   override def partOne(): String = slidingIncrease(1).toString
 
