64855b58d01711041552413da5e1b704a4ad11c1
Jonathan Ostrander <jonathanost@gmail.com>
2021-12-03 08:51:54 -0500

    feat: day three solution

---
+++ src/main/scala/aoc/Day03.scala
@@ -0,0 +1,43 @@
+package aoc
+
+object Day03 extends InputReader(3) with Day {
+  def bit(values: List[String], pos: Int, mostCommon: Boolean): Char = {
+    val (zeroes, ones) = values.map(_(pos)).foldLeft((0, 0)) {
+      case ((z, o), '0') => (z + 1, o)
+      case ((z, o), '1') => (z, o + 1)
+      case _ => sys.error("String is not binary")
+    }
+    if (mostCommon) {
+      if (ones >= zeroes) '1'
+      else '0'
+    } else {
+      if (zeroes <= ones) '0'
+      else '1'
+    }
+  }
+
+  implicit class BinaryString(val value: String) extends AnyVal { 
+    def *(other: String): String = (Integer.parseInt(value, 2) * Integer.parseInt(other, 2)).toString
+  }
+
+  def partOne(): String = {
+    val gamma = (0 until input.head.length).map(pos => bit(input, pos, true)).mkString
+    val epsilon = (0 until input.head.length).map(pos => bit(input, pos, false)).mkString
+    gamma * epsilon
+  }
+  def partTwo(): String = {
+    def loop(left: List[String], pos: Int, mostCommon: Boolean): String = left match {
+      case Nil => sys.error("Empty list")
+      case head :: Nil => head
+      case _ =>
+        val nextBit = bit(left, pos, mostCommon)
+        val nextLeft = left.filter(_(pos) == nextBit)
+        loop(nextLeft, pos + 1, mostCommon)
+    }
+
+    val oxy = loop(input, 0, mostCommon = true)
+    val co2 = loop(input, 0, mostCommon = false)
+
+    oxy * co2
+  }
+}
