e70c488d1a6f2ec828ea26c6c4ba822876316204
Jonathan Ostrander <jonathanost@gmail.com>
2021-12-08 09:51:36 -0500

    chore: clean-up

--- src/main/scala/aoc/Day08.scala
+++ src/main/scala/aoc/Day08.scala
@@ -8,14 +8,19 @@ package aoc
  *  6
  */
 object Day08 extends InputReader(8) with Day {
-  val unique = Set(2, 3, 4, 7)
-  override def partOne(): String = {
-    input
-      .flatMap(_.dropWhile(_ != '|').drop(2).split(" ").toList)
-      .filter(s => unique(s.length))
-      .length
-      .toString
+  val splitInput = input.map { line =>
+    (
+      line.takeWhile(_ != '|').trim.split(" ").toList.map(_.toSet),
+      line.dropWhile(_ != '|').drop(2).split(" ").toList
+    )
   }
+
+
+  val numbers =
+    List("012456","25","02346","02356","1235","01356","013456","025","0123456","012356")
+      .map(_.map(_.toString.toInt).toSet)
+      .zipWithIndex
+      .toMap
 
   def deduce(signals: List[Set[Char]]): Map[Char, Int] = {
     val one = signals.find(_.size == 2).get
@@ -24,13 +29,10 @@ object Day08 extends InputReader(8) with Day {
     val eight = signals.find(_.size == 7).get
 
     val top = seven.diff(one).head
-
     val topRight = one.find(c => signals.filter(_(c)).length == 8).get
     val bottomRight = (one - topRight).head
-
     val middle = four.diff(one).find(c => signals.filter(_(c)).length == 7).get
     val topLeft = (four.diff(one) - middle).head
-
     val bottomLeft = eight.find(c => signals.filter(_(c)).length == 4).get
 
     val allButBottom = List(top, topLeft, topRight, middle, bottomLeft, bottomRight)
@@ -40,26 +42,24 @@ object Day08 extends InputReader(8) with Day {
     (allButBottom ++ List(bottom)).zipWithIndex.toMap
   }
 
-  val numbers = Map(
-     "012456" -> 0,
-     "25" -> 1,
-     "02346" -> 2,
-     "02356" -> 3,
-     "1235" -> 4,
-     "01356" -> 5,
-     "013456" -> 6,
-     "025" -> 7,
-     "0123456" -> 8,
-     "012356" -> 9,
-  )
+    
+  override def partOne(): String = {
+    val unique = numbers.keys.toList.map(_.size).groupBy(identity).filter(_._2.size == 1).keySet
+    input
+      .flatMap(_.dropWhile(_ != '|').drop(2).split(" ").toList)
+      .filter(s => unique(s.length))
+      .length
+      .toString
+  }
 
   override def partTwo(): String =
-    input.map { line =>
-      val sigs = line.takeWhile(_ != '|').trim.split(" ").toList.map(_.toSet)
-      val nums = line.dropWhile(_ != '|').drop(2).split(" ").toList
-
-      val mapping = deduce(sigs)
-
-      nums.map(num => numbers(num.map(c => mapping(c)).sorted.mkString)).mkString("").toInt
+    splitInput.map {
+      case (sigs, nums) =>
+        val mapping = deduce(sigs)
+        nums
+          .map(_.map(c => mapping(c)).toSet)
+          .map(set => numbers(set))
+          .mkString
+          .toInt
     }.sum.toString
 }
