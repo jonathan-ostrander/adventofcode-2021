ae5c590d0f79b66ca3c5cef9748b8a1724cc3e9d
Jonathan Ostrander <jonathanost@gmail.com>
2021-12-10 08:09:44 -0500

    feat: day 10 solution (#13)

--- src/main/scala/aoc/DayRunner.scala
+++ src/main/scala/aoc/DayRunner.scala
@@ -5,7 +5,7 @@ import scala.reflect.runtime.universe
 object DayRunner extends App {
   val day = args(0)
   val rm = universe.runtimeMirror(getClass.getClassLoader)
-  val objectName = if (day.toInt < 10) s"Day0$day" else s"aoc.Day$day"
+  val objectName = if (day.toInt < 10) s"Day0$day" else s"Day$day"
   
   val dayObject: Day =
     rm.reflectModule(rm.staticModule(s"aoc.$objectName").asModule).instance.asInstanceOf[aoc.Day]
