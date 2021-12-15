98d6019771e1574544986ed0edc43e2529795272
Jonathan Ostrander <jonathanost@gmail.com>
2021-12-15 15:29:46 -0500

    feat: run today's solution if no argument is specified

--- src/main/scala/aoc/DayRunner.scala
+++ src/main/scala/aoc/DayRunner.scala
@@ -1,15 +1,24 @@
 package aoc
 
+import java.time.Instant
+import java.util.Calendar
 import scala.reflect.runtime.universe
+import scala.util.Try
 
 object DayRunner extends App {
-  val day = args(0)
+  val day = Try(args(0)).toOption.getOrElse(Calendar.getInstance().get(Calendar.DAY_OF_MONTH).toString)
   val rm = universe.runtimeMirror(getClass.getClassLoader)
   val objectName = if (day.toInt < 10) s"Day0$day" else s"Day$day"
   
   val dayObject: Day =
     rm.reflectModule(rm.staticModule(s"aoc.$objectName").asModule).instance.asInstanceOf[aoc.Day]
 
-  println(s"Part 1: ${dayObject.partOne()}")
-  println(s"Part 2: ${dayObject.partTwo()}")
+  def time(f: => Unit): Unit = {
+    val start = Instant.now
+    f
+    println(s"Took: ${Instant.now.toEpochMilli() - start.toEpochMilli()} ms")
+  }
+
+  time(println(s"Part 1: ${dayObject.partOne()}"))
+  time(println(s"Part 2: ${dayObject.partTwo()}"))
 }
